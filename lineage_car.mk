d818a544e032cc42d6c86342ae7d35ead719102c
Yuncheol Heo <ycheo@google.com>
2022-11-22 14:39:47 -0800

    car: Disable the optimazation of System Server in phone_car.
    - The optimazation removes the unused hidden apis in System Server,
      which can cause NoSuchMethodError exception on the access from
      CarServiceHelperService.
    
    Bug: 259601271
    Test: build phone_car and check if it boots.
    Change-Id: I8e5c5e6b461c55559b147e90b83e798bc6e563d3

--- lineage_car.mk
+++ lineage_car.mk
@@ -16,3 +16,7 @@ PRODUCT_PACKAGES += \
 # Whitelisted packages per user type
 PRODUCT_COPY_FILES += \
     device/google/cuttlefish/shared/auto/preinstalled-packages-product-car-cuttlefish.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-product-car-cuttlefish.xml
+
+# Workarounds
+## CarServiceHelperService accesses the hidden api in the system server.
+SYSTEM_OPTIMIZE_JAVA := false
