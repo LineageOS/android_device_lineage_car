bc57ffa18b2586d1a3e5a745cc08c05aeb920620
Bruno Martins <bgcngm@gmail.com>
2024-01-20 01:22:24 +0000

    Revert "car: Set PRODUCT_IS_AUTO to true"
    This reverts commit 78fba94fa922f76d578f8e544a0bad83dc198680.
    
    There's already PRODUCT_IS_AUTOMOTIVE, so use it instead.
    
    Change-Id: Id66e534893aa5b6a6ff4b412ba67b7cf938af6db

--- lineage_car.mk
+++ lineage_car.mk
@@ -4,5 +4,3 @@ PRODUCT_PACKAGE_OVERLAYS += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     config.override_forced_orient=true \
     persist.bluetooth.enablenewavrcp=false \
-
-PRODUCT_IS_AUTO := true
