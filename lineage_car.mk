5bfba3c73db5ee54a403e75d102f7772401132e6
Bruno Martins <bgcngm@gmail.com>
2024-01-21 23:32:48 +0000

    car: Convert current overlays into RRO packages
    These didn't produce the fully desired effect, because the AOSP
    auto-generated ones had more priority.
    
    Change-Id: I08478ae4c9567d53e1d4909ce74f99594d70758a

--- lineage_car.mk
+++ lineage_car.mk
@@ -1,5 +1,6 @@
-PRODUCT_PACKAGE_OVERLAYS += \
-    device/lineage/car/overlay \
+PRODUCT_PACKAGES += \
+    CarServiceOverlay \
+    CarSettingsProviderOverlay
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.bluetooth.enablenewavrcp=false \
