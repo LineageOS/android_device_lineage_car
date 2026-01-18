d1c70d2326f8e27f1147cf00e0c10b830cf5cf56
Michael Bestas <mkbestas@lineageos.org>
2026-01-18 05:52:54 +0200

    car: Add missing copyright headers
    Test: pipx run reuse lint
    Change-Id: I6095a7cbe4447b99f46e4837517b6c90f7216b34

--- lineage_car_vendor.mk
+++ lineage_car_vendor.mk
@@ -1,3 +1,8 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
 PRODUCT_PACKAGES += \
     android.hardware.automotive.audiocontrol-service.example \
     android.hardware.automotive.vehicle@V4-default-service
