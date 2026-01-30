14f865afa29c6e4569a31e0d0c7d32ae1540d080
Yumi Yukimura <me.cafebabe@gmail.com>
2026-01-31 01:07:07 +0800

    car: Inherit from AOSP Car product makefiles
    Inheriting just car.mk isn't enough to even basically function now,
    some necessary components are only defined on those product makefiles
    
    This follows whatever device/google_car and cuttlefish does.
    
    Change-Id: Ic3ddd6c4461728c001044a7b46be5526bc352145

--- lineage_car_vendor.mk
+++ lineage_car_vendor.mk
@@ -3,6 +3,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from AOSP Car product makefiles
+$(call inherit-product, packages/services/Car/car_product/build/car_vendor.mk)
+
 # HALs
 PRODUCT_PACKAGES += \
     android.hardware.automotive.audiocontrol-service.example \
