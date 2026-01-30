0f3b40241c5ceff1be0d19dd41bffca028e7c75c
Yumi Yukimura <me.cafebabe@gmail.com>
2026-01-31 01:19:15 +0800

    car: Tidy up the makefiles
    * Add section name
    * Entries that installs file to product partition
      should not belong to makefile for vendor partition
    
    Change-Id: I52dcf3c7ce4558c7a6a0b4f81b96ab5110a28f38

--- lineage_car.mk
+++ lineage_car.mk
@@ -3,6 +3,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Overlays
 PRODUCT_PACKAGES += \
     CarServiceOverlay \
     CarSettingsProviderOverlay
+
+# Whitelisted packages per user type
+PRODUCT_COPY_FILES += \
+    device/google/cuttlefish/shared/auto/preinstalled-packages-product-car-cuttlefish.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-product-car-cuttlefish.xml
--- lineage_car_vendor.mk
+++ lineage_car_vendor.mk
@@ -3,13 +3,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# HALs
 PRODUCT_PACKAGES += \
     android.hardware.automotive.audiocontrol-service.example \
     android.hardware.automotive.vehicle@V4-default-service
 
-# Additional selinux policy
+# SELinux
 BOARD_SEPOLICY_DIRS += device/google_car/common/sepolicy
-
-# Whitelisted packages per user type
-PRODUCT_COPY_FILES += \
-    device/google/cuttlefish/shared/auto/preinstalled-packages-product-car-cuttlefish.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-product-car-cuttlefish.xml
