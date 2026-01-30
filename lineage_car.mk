aa3698bc0d26c9c3991932ba2dc0c7d2acb558ab
Yumi Yukimura <me.cafebabe@gmail.com>
2026-01-31 01:43:55 +0800

    car: Import `PRODUCT_ENFORCE_RRO_TARGETS` workaround from cuttlefish
    Change-Id: I58f70f94888cde12974d35ed07530e335008d45e

--- lineage_car.mk
+++ lineage_car.mk
@@ -20,3 +20,6 @@ PRODUCT_COPY_FILES += \
 # Workarounds
 ## CarServiceHelperService accesses the hidden api in the system server.
 SYSTEM_OPTIMIZE_JAVA := false
+## FIXME: generic_system.mk sets 'PRODUCT_ENFORCE_RRO_TARGETS := *'
+##        but this breaks phone_car. So undo it here.
+PRODUCT_ENFORCE_RRO_TARGETS := frameworks-res
