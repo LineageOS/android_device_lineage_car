8c11d9896cc83b723de013fe093550cc8903a0d5
Yumi Yukimura <me.cafebabe@gmail.com>
2026-01-31 02:08:52 +0800

    car: Do not enforce artifact path requirements
    Breaks build
    
    Change-Id: I48ef0ab2c80d04cf498aff8b70ffb5830923f72a

--- lineage_car.mk
+++ lineage_car.mk
@@ -16,3 +16,7 @@ PRODUCT_PACKAGES += \
 # Whitelisted packages per user type
 PRODUCT_COPY_FILES += \
     device/google/cuttlefish/shared/auto/preinstalled-packages-product-car-cuttlefish.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-product-car-cuttlefish.xml
+
+# Workarounds
+## Do not enforce artifact path requirements
+PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := false
